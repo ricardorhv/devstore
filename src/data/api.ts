47caf5cc1ce74a43128c21febfd9b94164f79e76
Ricardo Henrique Vinciguerra <ricardorhv.dev@gmail.com>
2023-12-07 11:32:26 -0300

    docs: add a comment to explain what it was done in the wrapper api

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,5 +1,12 @@
 import { env } from '@/env'
 
+/*
+As the api fetch from NextJS doesn't have a baseurl, we wrapper
+the api and add it.
+It was created a env.ts to validate the data that is coming from 
+the environment variables 
+*/
+
 export function api(path: string, init?: RequestInit) {
   const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
   const url = new URL(path, baseUrl)
